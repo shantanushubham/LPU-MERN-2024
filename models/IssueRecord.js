5c30e699f4070b6f2b521a16b9148aafa90ebbd4
Shantanu Shubham <shantanu0707@gmail.com>
2024-06-27 09:57:43 +0530

    [Add] Issue Record Model

---
+++ models/IssueRecord.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const IssueRecordSchema = mongoose.Schema({
+  studentRegNo: {
+    type: Number,
+    required: true,
+  },
+  bookIsbnNo: {
+    type: Number,
+    required: true,
+    // TODO: Lets talk about validation later!
+  },
+  issueDate: {
+    type: Date,
+    default: Date.now,
+  },
+  returnDate: {
+    type: Date,
+    default: ({ issueDate }) => {
+      return new Date(issueDate.getTime() + 15 * 24 * 60 * 60 * 1000);
+    },
+  },
+  status: {
+    type: String,
+    enum: ["Issued", "Returned"],
+    default: "Issued",
+  },
+  lateFine: {
+    type: Number,
+    default: 0,
+    min: 0,
+  },
+});
+
+const IssueRecordModel = mongoose.model("issue-record", IssueRecordSchema);
+module.exports = IssueRecordModel;
